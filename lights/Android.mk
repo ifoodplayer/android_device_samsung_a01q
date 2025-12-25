2c1edec4ca8557506c9936a013fea91d04afcfb7
ifoodplayer <joaogamer1437@gmail.com>
2025-12-25 14:53:15 -0300

    m01q: Change the platform from lights.msm8953 to 8937
    It needs to be tested.

--- lights/Android.mk
+++ lights/Android.mk
@@ -26,7 +26,7 @@ ifeq ($(LLVM_SA), true)
     LOCAL_CFLAGS += --compile-and-analyze --analyzer-perf --analyzer-Werror
 endif
 LOCAL_CLANG  := true
-LOCAL_MODULE := lights.msm8953
+LOCAL_MODULE := lights.msm8937
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
