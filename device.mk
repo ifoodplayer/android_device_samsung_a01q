8e2e7e005f9590d20b12b27c3cbe019848d905af
ifoodplayer <joaogamer1437@gmail.com>
2025-12-30 10:55:42 -0300

    m01q: Import init.vendor.rilchip.rc

--- device.mk
+++ device.mk
@@ -43,6 +43,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     fstab.boot.qcom \
     fstab.qcom \
+    init.vendor.rilchip.rc \
 	init.hq.common.rc \
 	init.hq.shipping.rc \
 	init.m01q.rc \
