707adfd45689032cbbcf0c80f3b7fc8b92719572
ifoodplayer <joaogamer1437@gmail.com>
2026-01-13 20:07:42 -0300

    a01q: Use prebuilt Audio HAL

--- device.mk
+++ device.mk
@@ -141,12 +141,10 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impla.a01q \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-service:32 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.a2dp.default \
-    audio.primary.msm8937 \
     audio.r_submix.default \
     audio.bluetooth.default \
     audio.usb.default \
