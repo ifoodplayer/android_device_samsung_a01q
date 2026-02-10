20b62934b3d0a1ad57627a124103db971ca5ebf8
ifoodplayer <joaogamer1437@gmail.com>
2026-02-10 14:46:43 -0300

    a01q: Remove debug flags

--- device.mk
+++ device.mk
@@ -374,16 +374,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
 
-# For userdebug builds
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	ro.secure=0 \
-	ro.adb.secure=0 \
-	ro.debuggable=1 \
-	persist.sys.root_access=1 \
-	persist.service.adb.enable=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=adb
-
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/a01q/a01q-vendor.mk)
